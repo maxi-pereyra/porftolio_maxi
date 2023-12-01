dbe66ffb0fa588adef7a3563a79c6617b755230f
maxi-pereyra <maxiabelpereyra@hotmail.com.ar>
2023-12-01 17:02:42 -0300

    up nav header

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -12,24 +12,12 @@ export const links = [
     hash: "#home",
   },
   {
-    name: "About",
-    hash: "#about",
-  },
-  {
     name: "Projects",
     hash: "#projects",
   },
   {
     name: "Skills",
     hash: "#skills",
-  },
-  {
-    name: "Experience",
-    hash: "#experience",
-  },
-  {
-    name: "Contact",
-    hash: "#contact",
   },
 ] as const;
 
